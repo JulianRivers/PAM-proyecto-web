52e14e4d25cb1407d9f2d69796a2b67783223605
Santiagomorge <santiagomorg27@gmail.com>
2022-11-05 20:17:27 -0500

    Modelar director

--- director/models.py
+++ director/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Director(models.Model) :
+    codigo = models.CharField('Codigo del director', primary_key=True, max_length=10)
+    nombres = models.CharField('Nombres del director', max_length=150, null=False)
+    apellidos = models.CharField('Apellidos del director', max_length=150, null=False)
+    correo = models.EmailField('Correo del director', max_length=254, unique=True, null=False)
+
+    def __str__(self):
+        return "Director: "+self.nombres
--- director/views.py
+++ director/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def head(request, nombres):
+    print(nombres)
+    return HttpResponse("<h1>PANEL DE DIRECTOR</h1>")
