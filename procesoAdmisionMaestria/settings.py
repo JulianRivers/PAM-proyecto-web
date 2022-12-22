1a5a6fc3df02bad000762bdf66d2609abe51056a
JulianRivers <juliancamilorifo@ufps.edu.co>
2022-12-21 20:16:14 -0500

    img matias y debug false

--- procesoAdmisionMaestria/settings.py
+++ procesoAdmisionMaestria/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ecpnpz=%nh_@%787fto#it&xf+m3hr+71%4s5!7j2s3jl=1a37'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
